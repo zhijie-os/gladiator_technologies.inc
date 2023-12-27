6523124b3e659abf1cabb79d8d33a4db1df130b4
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2023-12-27 16:46:22 +0800

    pushed

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="/contact"
-                    className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
+                    className="inline-block rounded-lg bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-slate-500 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     联系我们
                   </Link>
